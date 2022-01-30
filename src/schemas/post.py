8d967b3e620bd5d4c35ac9cba57f9aacc4913bfe
Sajidur Rahman <sajidur.inbox@gmail.com>
2022-01-30 23:26:53 +0600

    pydentic config

--- src/schemas/post.py
+++ src/schemas/post.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
 
+
 class PostIn(BaseModel):
     title: str
     body: str
+
 
 class PostOut(BaseModel):
     id: int
     title: str
     body: str
+
+    class Config:
+        orm_mode = True
