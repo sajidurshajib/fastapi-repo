5a449552d60ea7df2115efdb3862508fd384eb7e
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-03 01:36:53 +0600

    n

--- src/main.py
+++ src/main.py
@@ -2,14 +2,15 @@ from fastapi import FastAPI
 from routes import (
     post_route,
     catagory_route
-    )
+)
 
 
 app = FastAPI()
 
+
 @app.get('/')
 async def index():
-    return {"msg":"Hello FastAPI"}
+    return {"msg": "Hello FastAPI"}
 
 
 app.include_router(catagory_route)
