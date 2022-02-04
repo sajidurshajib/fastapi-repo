b79774964aedf7516cee33029c786a9e2d680167
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-04 20:23:37 +0600

    added: main module check

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from imp import reload
+import uvicorn
 from fastapi import FastAPI
 from routes import (
     post_route,
@@ -15,3 +17,8 @@ async def index():
 
 app.include_router(catagory_route)
 app.include_router(post_route.router)
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1",
+                port=8000, reload=True, log_level="info")
