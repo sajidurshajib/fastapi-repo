e4e1492af04545879ba5ba7872bd9e2be97d5ec8
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-04 20:23:57 +0600

    added: crud operations

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,7 +1,9 @@
+from sqlalchemy import delete, update
+from sqlalchemy.future import select
 from pydantic import BaseModel
 from db.config import Base
 from repositories import ABSRepo
-from typing import Generic, Type, TypeVar
+from typing import Generic, Type, TypeVar, Union
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.encoders import jsonable_encoder
 
@@ -22,3 +24,31 @@ class Repo(Generic[ModelType, create_schema], ABSRepo):
         await db.commit()
         await db.refresh(db_obj)
         return db_obj
+
+    async def read(self, db: AsyncSession) -> ModelType:
+        query = select(self.model)
+        result = await db.execute(query)
+        c = result.scalars()
+        return list(c)
+
+    async def update(self, db: AsyncSession, id: int, update_in: create_schema) -> ModelType:
+        query = update(self.model).where(self.model.id == id).values(
+            **update_in.dict(exclude_unset=True))
+        result = await db.execute(query)
+        await db.commit()
+        data = None
+        if result.rowcount:
+            data = await self.get_by_id(db, id)
+        return data
+
+    async def delete(self, db: AsyncSession, id: Union[str, int]) -> ModelType:
+        query = delete(self.model).where(self.model.id == id)
+        result = await db.execute(query)
+        await db.commit()
+        return result
+
+    async def get_by_id(self, db: AsyncSession, id: int) -> ModelType:
+        query = select(self.model).filter_by(id=id).limit(1)
+        result = await db.execute(query)
+        c = result.scalars().first()
+        return c
