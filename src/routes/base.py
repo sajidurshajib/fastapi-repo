36470dcf8db1dd2ccc5eba7579ea50d035876688
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-04 20:24:16 +0600

    added: crud operations

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,4 +1,5 @@
 from typing import List
+from unittest import result
 from repositories import Repo
 from pydantic import BaseModel
 from routes import ABSRoute
@@ -27,33 +28,30 @@ class BaseRoute(ABSRoute):
 
     def get(self):
         @self.router.get('', response_model=List[self.response_schema])
-        async def get():
-            return [{"id": 1,
-                     "title": "Demo",
-                     "body": "Demo body"}]
+        async def get(db=Depends(get_session)):
+            read = await self.repo.read(db)
+            return read
 
     def post(self):
         @self.router.post('', response_model=self.response_schema)
         async def post(data_in: self.request_schema, db=Depends(get_session)):
             result = await self.repo.create(db, data_in)
-            print(result)
             return result
 
     def get_one(self):
         @self.router.get('/{id}', response_model=self.response_schema)
-        async def get(id: int):
-            return {"id": 1,
-                    "title": "Demo",
-                    "body": "Demo body"}
+        async def get(id: int, db=Depends(get_session)):
+            single_result = await self.repo.get_by_id(db, id)
+            return single_result
 
     def put(self):
         @self.router.put('/{id}', response_model=self.response_schema)
-        async def put(id: int, update_in: self.request_schema):
-            return {"id": 1,
-                    "title": "Demo",
-                    "body": "Demo body"}
+        async def put(id: int, update_in: self.request_schema, db=Depends(get_session)):
+            update_result = await self.repo.update(db, id, update_in)
+            return update_result
 
     def delete(self):
         @self.router.delete('/{id}')
-        async def delete(id: int):
-            return {'msg': 'deleted'}
+        async def delete(id: int, db=Depends(get_session)):
+            await self.repo.delete(db, id)
+            return "Deleted"
