7aec3882fc3b4ae3074c15b12b4b2b579d906cdd
sajidurshajib <sajidur.inbox@gmail.com>
2022-02-18 13:52:38 +0600

    implemet: Jinja2 and staticFile

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-from imp import reload
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from routes import (
     post_route,
     catagory_route
@@ -10,9 +12,14 @@ from routes import (
 app = FastAPI()
 
 
-@app.get('/')
-async def index():
-    return {"msg": "Hello FastAPI"}
+app.mount('/static', StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
+
+
+@app.get('/', response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse('index.html', {"request": request})
 
 
 app.include_router(catagory_route)
